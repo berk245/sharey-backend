42b955a1726f75edeba16c7f5938c9245bf429ed
berk.ozzambak <berkozzambak@gmail.com>
2023-07-18 21:46:01 +0200

    Create a config file to establish db connection

---
+++ database/config.js
@@ -0,0 +1,24 @@
+const path = require("path");
+require("dotenv").config({ path: path.join(__dirname, "..", ".env") });
+
+const Sequelize = require("sequelize");
+
+const db = new Sequelize(
+  process.env.MYSQL_DATABASE,
+  process.env.MYSQL_USER,
+  process.env.MYSQL_PASSWORD,
+  {
+    host: process.env.MYSQL_HOST,
+    dialect: "mysql",
+  }
+);
+
+db.authenticate()
+  .then(() => {
+    console.log("Connected to db successfuly");
+  })
+  .catch((err) =>
+    console.log(err)
+  );
+
+module.exports = db;
