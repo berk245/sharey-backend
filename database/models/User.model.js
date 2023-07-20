d9206b19fcd08ea97924bcedcfe28a4ec9f5710e
berk.ozzambak <berkozzambak@gmail.com>
2023-07-20 22:51:56 +0200

    Add user model to models folder

---
+++ database/models/User.model.js
@@ -0,0 +1,61 @@
+const { Sequelize, DataTypes } = require("sequelize");
+const db = require("../config/database");
+
+const User = db.define(
+  "User",
+  {
+    user_id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    last_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    phone: {
+      type: DataTypes.STRING(12),
+      allowNull: false,
+    },
+    city: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    address: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    member_since: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+    },
+    profile_photo_url: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    is_active: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: true
+    },
+  },
+  {
+    tableName: "User",
+    timestamps: false,
+  }
+);
+
+module.exports = User;
